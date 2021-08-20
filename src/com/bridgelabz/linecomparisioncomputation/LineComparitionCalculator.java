dc2ede65f61e47081455463dfbbb65bff481fa8c
AshikaChadaga <ashikachadaga@gmail.com>
2021-08-20 12:59:55 +0530

    ADDED: Welcome message to master

---
+++ src/com/bridgelabz/linecomparisioncomputation/LineComparitionCalculator.java
@@ -0,0 +1,11 @@
+package com.bridgelabz.linecomparisioncomputation;
+
+public class LineComparitionCalculator {
+
+	public static void main(String[] args) {
+		
+		System.out.println("------------- Welcome to Line Comparision Computation! --------------");
+
+	}
+
+}
